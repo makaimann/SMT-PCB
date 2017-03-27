6f968c0b5e7de42de2c1634caf12c81cfac88611
Steven Herbst <sgherbst@gmail.com>
2017-03-27 14:58:57 -0700

    freerouting partially working from the command line

--- smt-pcb/run_freerouting.py
+++ smt-pcb/run_freerouting.py
@@ -4,6 +4,7 @@ import subprocess
 import argparse
 import os
 import os.path
+import time
 
 def main():
     # load command-line arguments
@@ -12,10 +13,19 @@ def main():
     parser.add_argument('--route_dir')
     args = parser.parse_args()
 
+    # find the DSN file
+    args.dsn = os.path.expanduser(args.dsn)
+    args.dsn = os.path.abspath(args.dsn)
+
     # run FreeRouting in the correct directory
     args.route_dir = os.path.expanduser(args.route_dir)
     args.route_dir = os.path.abspath(args.route_dir)
-    subprocess.Popen(['java', '-jar', 'FreeRouter.jar'], cwd=args.route_dir)
+    jar_cmd = ['java', '-jar', 'FreeRouter.jar', 
+               '-de', args.dsn,
+               '-di', os.path.dirname(args.dsn)]
+    print jar_cmd
+    proc = subprocess.Popen(jar_cmd, cwd=args.route_dir)
+    proc.wait()
 
 if __name__ == '__main__':
     main()
