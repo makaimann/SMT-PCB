4d8610b22b7619778f1a851321483917ce749b92
Steven Herbst <sgherbst@gmail.com>
2017-03-27 20:46:44 -0700

    export_specctra mostly working except for net classes

--- smt-pcb/export_specctra.py
+++ smt-pcb/export_specctra.py
@@ -97,8 +97,8 @@ def boundary(edge):
     for point in edge:
         # convert points from mm to um
         # and negate y coordinate
-        x = '%d' %  (point.x*1000)
-        y = '%d' % (-point.y*1000)
+        x = '%d' % dsnx(point.x)
+        y = '%d' % dsny(point.y)
         path = path[:] + [x, y]
 
     return ['boundary', path]
@@ -123,8 +123,8 @@ def placement(mod_list):
         # interpret information from this module's entry
         pn = mod['name']
         refdes = mod['refdes']
-        x = '%d' % ( mod['x']*1000)
-        y = '%d' % (-mod['y']*1000)
+        x = '%d' % dsnx(mod['x'])
+        y = '%d' % dsny(mod['y'])
         rot = '%d' % (mod['rot'])
 
         if mod['layer'] == 'F.Cu':
@@ -134,12 +134,12 @@ def placement(mod_list):
         else:
             raise Exception('Invalid component placement')
 
-        place = ['place', refdes, x, y, side, rot, ['PN', pn]]
+        place = ['place', refdes, x, y, side, rot, ['PN', dsns(pn)]]
         comp_dict[mod['name']].append(place)
 
     # format placement
     for comp, comp_mods in comp_dict.items():
-        info.append(['component', comp] + comp_mods)
+        info.append(['component', dsns(comp)] + comp_mods)
 
     return info
 
@@ -159,7 +159,7 @@ def library(tree):
                 pad_set = pad_set.union(pads)
     
     # add default via to set of pads
-    pad_set.add('"Via[0-1]_800:400_um"')
+    pad_set.add('Via[0-1]_800:400_um')
 
     # make a library entry for each unique type of pad
     for pad_name in pad_set:
@@ -181,11 +181,12 @@ def network(design):
     # add each net to the network
     for net, pin_list in net_dict.items():
         pins = ['pins'] + pin_list
-        entry = ['net', net, pins]
+        entry = ['net', dsns(net), pins]
         network.append(entry)
 
     # add default routing rules
-    rclass = ['kicad_default', '""'] + net_dict.keys()
+    all_nets = [dsns(x) for x in net_dict.keys()]
+    rclass = ['kicad_default', '""'] + all_nets
     rclass.append(['circuit', ['use_via', 'Via[0-1]_800:400_um']])
     rclass.append(['rule', ['width', '250'], ['clearance', '200.1']])
     
@@ -197,7 +198,7 @@ def wiring():
     return ['wiring']
 
 def make_comp(entry):
-    comp = ['image', entry[1]]
+    comp = ['image', dsns(entry[1])]
     outline = []
     pins = []
     pad_set = set()
@@ -205,13 +206,13 @@ def make_comp(entry):
         if val[0] == 'fp_line':
             for arg in val:
                 if arg[0] == 'start':
-                    startx = '%d' % (int(1000*float(arg[1])))
-                    starty = '%d' % (int(-1000*float(arg[2])))
+                    startx = '%d' % dsnx(arg[1])
+                    starty = '%d' % dsny(arg[2])
                 elif arg[0] == 'end':
-                    endx = '%d' % (int(1000*float(arg[1])))
-                    endy = '%d' % (int(-1000*float(arg[2])))
+                    endx = '%d' % dsnx(arg[1])
+                    endy = '%d' % dsny(arg[2])
                 elif arg[0] == 'width':
-                    width = '%d' % (int(1000*float(arg[1])))
+                    width = '%d' % scale(arg[1])
             line = ['outline', ['path', 'signal', width, startx, starty, endx, endy]]
             outline = outline + [line]
         elif val[0] == 'pad':
@@ -219,46 +220,100 @@ def make_comp(entry):
             pad_type = val[3]
             for arg in val:
                 if arg[0] == 'size':
-                    pad_size = int(1000*float(arg[1]))
-                if arg[0] == 'at':
-                    x = '%d' % (int(1000*float(arg[1])))
-                    y = '%d' % (int(-1000*float(arg[2])))
-            fr_pad_name = make_pad_name(pad_type, pad_size)
+                    pad_size = (scale(arg[1]), scale(arg[2]))
+                elif arg[0] == 'at':
+                    x = '%d' % dsnx(arg[1])
+                    y = '%d' % dsny(arg[2])
+                elif arg[0] == 'layers':
+                    layers = arg[1:]
+                    if '*.Cu' in layers:
+                        pad_layer = 'A'
+                    elif 'F.Cu' in layers:
+                        pad_layer = 'T'
+                    elif 'B.Cu' in layers:
+                        pad_layer = 'B'
+                    else:
+                        raise Exception('Invalid pad layer')
+            fr_pad_name = make_pad_name(pad_type, pad_layer, pad_size)
             pad_set.add(fr_pad_name)
-            pad = ['pin', fr_pad_name, pad_name, x, y]
+            pad = ['pin', dsns(fr_pad_name), pad_name, x, y]
             pins.append(pad)
     
     comp = comp + outline + pins
     return comp, pad_set
 
-def make_pad_name(pad_type, pad_size):
+def make_pad_name(pad_type, pad_layer, pad_size):
     if pad_type == 'oval':
-        return 'Oval[A]Pad_%dx%d_um' % (pad_size, pad_size)
+        return 'Oval[%s]Pad_%dx%d_um' % (pad_layer, pad_size[0], pad_size[1])
+    elif pad_type == 'rect':
+        return 'Rect[%s]Pad_%dx%d_um' % (pad_layer, pad_size[0], pad_size[1])
     elif pad_type == 'circle':
-        return 'Round[A]Pad_%d_um' % (pad_size)
+        return 'Round[%s]Pad_%d_um' % (pad_layer, pad_size[0])
     else:
         raise Exception('Unsupported pad type.')
 
 def make_pad(name):
-    types = '(Round\[A\]Pad|Oval\[A\]Pad|\"Via\[0-1\])'
-    size = '([0-9]+)[_x:]'
-    p = re.compile(types + '_' + size)
+    types = '(Round|Oval|Rect|Via)'
+    layers = '\[([ATB]|0-1)\](Pad)?_'
+    size = '(\\d+x\\d+|\\d+:\\d+|\\d+)'
+    units = '_um'
+    p = re.compile(types + layers + size + units)
     m = p.match(name)
     if not m:
+        print p
+        print name
         raise Exception('Invalid pad name.')
     else:
-        size_str = m.group(2)
-        pad = ['padstack', name]
-        if m.group(1).startswith('Round') or m.group(1).startswith('\"Via'):
-            pad.append(['shape', ['circle', 'F.Cu', size_str]])
-            pad.append(['shape', ['circle', 'B.Cu', size_str]])
-        elif m.group(1).startswith('Oval'):
-            pad.append(['shape', ['path', 'F.Cu', size_str, '0', '0', '0', '0']])
-            pad.append(['shape', ['path', 'B.Cu', size_str, '0', '0', '0', '0']])
+        pad = ['padstack', dsns(name)]
+        if m.group(1) in ['Round']:
+            size_str = m.group(4)
+            def shape_gen(side):
+                return ['shape', ['circle', side, size_str]]
+        elif m.group(1) in ['Via']:
+            dia, drill = m.group(4).split(':')
+            dia, drill = int(dia), int(drill)
+            size_str = '%d' % dia 
+            def shape_gen(side):
+                return ['shape', ['circle', side, size_str]]
+        elif m.group(1) in ['Oval']:
+            w, h = m.group(4).split('x')
+            w, h = int(w), int(h)
+            size_str = '%d' % w # TODO: are there any cases where w != h?
+            def shape_gen(side):
+                return ['shape', ['path', side, size_str, '0', '0', '0', '0']]
+        elif m.group(1) in ['Rect']:
+            w, h = m.group(4).split('x')
+            w, h = int(w), int(h)
+            llx = '%d' % (-w/2)
+            lly = '%d' % (-h/2)
+            urx = '%d' % (w/2)
+            ury = '%d' % (h/2)
+            def shape_gen(side):
+                return ['shape', ['rect', side, llx, lly, urx, ury]]
         else:
             raise Exception('Invalid pad type.')
+        if m.group(2) in ['T', 'A', '0-1']:
+            pad.append(shape_gen('F.Cu'))
+        if m.group(2) in ['B', 'A', '0-1']:
+            pad.append(shape_gen('B.Cu'))
         pad.append(['attach', 'off'])
         return pad
+
+def scale(val):
+    return int(1000*float(val))
+
+def dsnx(x):
+    return scale(x)
+
+def dsny(y):
+    return -scale(y)
+
+def dsns(val):
+    val = val.replace('"', '')
+    if '-' in val:
+        return '"' + val + '"'
+    else:
+        return val
 
 if __name__ == '__main__':
     main()
