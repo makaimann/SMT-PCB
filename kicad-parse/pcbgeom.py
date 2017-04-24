8dac5b2bad3643a69fc726303d7e98e3af5373fa
Steven Herbst <sherbst@stanford.edu>
2017-04-24 16:45:09 -0700

    top and bottom handled properly

---
+++ kicad-parse/pcbgeom.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# Steven Herbst
+# sherbst@stanford.edu
+
+# Library enables geometry operations for PCBs
+
+from math import cos, sin
+
+class BoundingBox:
+    def __init__(self):
+        inf = float('inf')
+        self.xmin = inf
+        self.xmax = -inf
+        self.ymin = inf
+        self.ymax = -inf
+
+    def add(self, x, y):
+        if x < self.xmin:
+            self.xmin = x
+        if x > self.xmax:
+            self.xmax = x
+        if y < self.ymin:   
+            self.ymin = y
+        if y > self.ymax:
+            self.ymax = y
+
+    @property
+    def width(self):
+        return self.xmax - self.xmin
+
+    @property
+    def height(self):
+        return self.ymax - self.ymin
+
+    @property
+    def center(self):
+        return (self.xmin + self.xmax)/2.0, (self.ymin + self.ymax)/2.0
+
+def formRect(w, h):
+    return [(0,0), (w,0), (w,h), (0,h)]
+
+def roundPoint(p):
+    return (round(p[0]), round(p[1]))
+
+def mult(p, scale):
+    return (scale*p[0], scale*p[1])
+
+def invertY(p):
+    return (+p[0], -p[1])
+
+def invertX(p):
+    return (-p[0], +p[1])
+
+def translate(p, x, y):
+    return (p[0]+x, p[1]+y)
+
+def rotate(p, th):
+    x = p[0]*cos(th) - p[1]*sin(th)
+    y = p[0]*sin(th) + p[1]*cos(th)
+    return (x, y)
+
+def transform(p, th, mirror, x, y):
+    # rotate
+    p = rotate(p, th)
+
+    # invert Y coordinate if on the bottom
+    if mirror:
+        p = invertY(p)
+
+    # translate by offset
+    p = translate(p, x, y)
+
+    # return point
+    return p
